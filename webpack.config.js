57774b29fdff3598dc854ad36f8ccf7da5f76b92
citrixorange <caio.cb.sp@gmail.com>
2024-01-06 10:04:27 -0300

    Hello World Extension

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            },
+        },
+    ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: 'manifest.json', to: 'manifest.json' },
+              { from: 'images', to: 'images' },
+            ],
+        }),
+    ],
+};
